d13697d5eacbd1666b9b2e5003bc7db98072fb5d
ISTOPENAPI <vinodkumar.nooli@ispatialtec.com>
2024-04-04 15:57:22 +0530

    config map server change

--- js/MapConfig.js
+++ js/MapConfig.js
@@ -1,7 +1,7 @@
 ﻿var portalUrl = "https://maps.smartgeoapps.com/portal"
 var TrustedDomains = ["enviroportal.ead.ae", "localhost", "atlas.smartgeoapps.com"];
 var WebMapID = "753b0e40332946c5a16c5f97cb8317d4";
-var MonitoringStationsAPI = "https://enviroportal.ead.ae/server/rest/services/Enviroportal/MonitoringStations/MapServer/0" //"https://atlas.smartgeoapps.com/server/rest/services/AIrQuality/MonitoringStations/MapServer/0";
+var MonitoringStationsAPI = "https://atlas.smartgeoapps.com/server/rest/services/AIrQuality/MonitoringStations/MapServer/0";
 //var AirQualityService1 = "https://atlas.smartgeoapps.com/AirQualityWS/AQMS.asmx";
 var AirQualityService = "https://adairqualityapi.ead.ae/";
 var ImagesPath = "./images/new-images/";
@@ -9,7 +9,7 @@ var stationsIDs= [
     {
         "Hamdan Street": "EAD_HamdanStreet",
         "Khadejah School": "EAD_KhadijaSchool",
-        "Khalifa School": "EAD_KhalifaSchool",
+        "Khalifa School": "EAD_KhalifaSchool",  
         "Mussafah": "EAD_Mussafah",
         "Baniyas School": "EAD_Baniyas",
         "Al Ain Islamic Institute": "EAD_AlAinSchool",
