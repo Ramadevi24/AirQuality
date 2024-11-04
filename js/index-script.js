5ea12de0d8e2fbdb313a72d82301ef2076d3345f
Ramadevi24 <ramadevi.taraka@ispatialtec.com>
2024-11-04 20:27:47 +0530

    graphs issue

--- js/index-script.js
+++ js/index-script.js
@@ -1383,13 +1383,20 @@ const aqiContentArabic = {
     VeryUnHealthly: "ليس وقتاً مناسباً للتواجد في الخارج، اقضِ يومك في الداخل.",
     Hazardous: "يجب عليك تجنب الأنشطة الخارجية ",
 }
-
 const chartFilter = {
-    Hourly: 'Hourly',
+    Hourly:  'Hourly',
     Daily: 'Daily',
-    Monthly: 'Monthly',
-    Yearly: 'Yearly',
+    Monthly:  'Monthly',
+    Yearly:  'Yearly',
     Custom: 'Custom'
+}
+
+const chartFilterArabic = {
+    Hourly:  'كل ساعة',
+    Daily:  'يومي',
+    Monthly:  'شهري',
+    Yearly:  'سنويا',
+    Custom:  'مخصص'
 }
 
 const pollutantNamesEnglish = {
@@ -1830,7 +1837,7 @@ $(document).ready(function () {
         $('.dropdown-change li a').removeClass("active");
         el.find('.quality-index-dropItem:first').addClass("active");
         $('.datepicker').val('');
-        updateCharts(chartFilter.Hourly);
+        updateCharts(chartFilter.Hourly);   
         // Remove show class when quality-index-dropItem is clicked
         $('.dropdown-change li a').click(function () {
             $('.dropdown-change').removeClass('show').css('top', '');
@@ -3195,10 +3202,10 @@ function getStationChartApi(filter, initialRequest = false) {
     var url;
     var station;
     switch (filter) {
-        case chartFilter.Daily:
+        case currentLanguage === 'arabic' ? chartFilterArabic.Daily : chartFilter.Daily:
             url = baseUrl + 'GetDailyStationChart?stationName=' + currentStationDetails.stationId;
             break;
-        case chartFilter.Monthly:
+        case currentLanguage === 'arabic' ? chartFilterArabic.Monthly: chartFilter.Monthly:
             var airQualityStationid = currentStationDetails.stationId;
             station = stationIdforEDB.find(station => station.stationName === currentStationDetails.stationId);
             if (station) {
@@ -3207,14 +3214,14 @@ function getStationChartApi(filter, initialRequest = false) {
             var edbStationid = currentStationDetails.stationId;
             url = baseUrl + 'GetMonthlyStationChart?airQualityStationid=' + airQualityStationid + '&edbStationid=' + edbStationid;
             break;
-        case chartFilter.Yearly:
+        case currentLanguage === 'arabic' ? chartFilterArabic.Yearly: chartFilter.Yearly:
             station = stationIdforEDB.find(station => station.stationName === currentStationDetails.stationId);
             if (station) {
                 currentStationDetails.stationId = station.stationid;
             }
             url = baseUrl + 'GetYearlyStationChart?stationId=' + currentStationDetails.stationId;
             break;
-        case chartFilter.Custom:
+        case currentLanguage === 'arabic' ? chartFilterArabic.Custom: chartFilter.Custom:
             var selectedDate = $('.datepicker').val();
             var splitDateArray = selectedDate.split('-');
             var formatedDate = splitDateArray[1] + '/' + splitDateArray[0] + '/' + splitDateArray[2];
@@ -3339,25 +3346,25 @@ function bindStationDataToLineChart(filter) {
     fianlItems = dataArray.filter(item => labelsToFind.includes(item.label));
     
     switch (filter) {
-        case chartFilter.Daily:
+        case currentLanguage === 'arabic' ? chartFilterArabic.Daily : chartFilter.Daily:
             //categoriesData = chartData.map(t => { return t.day.split(' '); });
             chartData.forEach(item => {
                 categoriesData.push(item.day.split(' '));
             });
             break;
-        case chartFilter.Monthly:
+            case currentLanguage === 'arabic' ? chartFilterArabic.Monthly : chartFilter.Monthly:
             chartData.forEach(item => {
                 categoriesData.push(item.month);
             });
             //categoriesData = chartData.map(t => { return t.month; });
             break;
-        case chartFilter.Yearly:
+            case currentLanguage === 'arabic' ? chartFilterArabic.Yearly :  chartFilter.Yearly:
             //categoriesData = chartData.map(t => { return t.year; });
             chartData.forEach(item => {
                 categoriesData.push(item.year);
             });
             break;
-        case chartFilter.Custom:
+            case currentLanguage === 'arabic' ? chartFilterArabic.Custom :  chartFilter.Custom:
             //categoriesData = chartData.map(t => { return t.hour.split(' '); });
             chartData.map(item => {
                 const dateParts = item.recordedDate.split('/');
@@ -5833,8 +5840,9 @@ function updateCharts(selectedFilter) {
     // Do not remove below code starts---------------------------------
     $("#lineChartAqiSo2Value, #lineChartAqiNo2Value, #lineChartAqiCoValue, #lineChartAqiPm10Value, #lineChartAqiPm25Value, #lineChartAqiO3Value").text('');
     $('button.quality-button-dropdown').text(selectedFilter);
-    if (selectedFilter != chartFilter.Custom) {
-        getStationChartApi(selectedFilter);
+    const filterData = currentLanguage === 'arabic' ? chartFilterArabic.Custom : chartFilter.Custom
+    if (selectedFilter != filterData) {
+         getStationChartApi(selectedFilter);
     }
     // Do not remove below code ends---------------------------------
 }
@@ -6915,6 +6923,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const iaqmElement = document.getElementById('iaqm');
     const switchingElement = document.getElementById('switching-air-purifier')
     // const pollutantLegendTextItems = document.querySelectorAll('.pollutants-legend-text .legend li .legend_radar li');
+    const lastRefreshTimeElement = document.getElementById('aqilastrefreshtime');
+    activePollutant = pollutantAbbrevations.AQI;
     currentLanguage = 'english';
     currentStatusClass = statusClass;
     getAirQualitySafetyLevel();
@@ -6927,7 +6937,6 @@ document.addEventListener("DOMContentLoaded", () => {
         toggleBtns[i].addEventListener('click', toggleLanguage);
     }
      function toggleLanguage() {
-
         if (currentLanguage === 'english') {
             currentLanguage = 'arabic';
             bindLiveCityRanking();
@@ -6941,8 +6950,9 @@ document.addEventListener("DOMContentLoaded", () => {
             // updateHealthRecommendations();
             updateAqitoArabic();
             document.body.setAttribute('dir', 'rtl');
-            updateToArabic();
-
+            updateToArabic(); 
+            lastRefreshTimeElement.innerText = showHideToggleDiv(activePollutant.toLowerCase() + 'Tab', activePollutant);
+            updateCharts(chartFilterArabic.Hourly);   
         } else {
             currentLanguage = 'english';
             bindLiveCityRanking();
@@ -6957,6 +6967,8 @@ document.addEventListener("DOMContentLoaded", () => {
             updateAqitoEnglish();
             document.body.setAttribute('dir', 'ltr');
             updateToEnglish();
+            lastRefreshTimeElement.innerText = showHideToggleDiv(activePollutant.toLowerCase() + 'Tab', activePollutant);
+            updateCharts(chartFilter.Hourly); 
         }        
            }
 
@@ -7122,7 +7134,6 @@ function updateToArabic() {
     document.querySelectorAll('.last-refersh-color').forEach(element => {
         element.innerText ='آخر تحديث'
     });
-    // document.querySelector('.last-refersh-color').innerText = 'آخر تحديث';
     document.querySelector('.last-refersh-color').style.direction = 'ltr';
     document.querySelector('.station-aqi-legend').style.direction='rtl';
     // document.getElementById('barChartFilter').innerText='كل ساعة';
