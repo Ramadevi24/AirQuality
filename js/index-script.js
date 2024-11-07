c7e266da7de201e9087aa29c2e63fa70a8cfd41c
sakethkowtharapu <kowtharapusaketh@gmail.com>
2024-11-07 15:47:07 +0530

    font family

--- js/index-script.js
+++ js/index-script.js
@@ -7060,6 +7060,7 @@ function updateToArabic() {
     renderAccordionContent(accordionArabicContent);
     loadCarousel(imageDataArabic);
     currentStatusClass = statusClassArabic;
+    document.body.classList.toggle('arabic-mode');
     $('.accordion-button').addClass('rtl-accordion');
     $('.faqscrolling').addClass('rtl-faqscrolling');
     $('.contact-info-content').addClass('contact-info-data');
@@ -7519,6 +7520,7 @@ document.querySelector('.station-aqi-pill').style.padding='0';
 //englishtransulation
 
 function updateToEnglish() {
+    document.body.classList.remove('arabic-mode');
     renderAccordionContent(accordionContent);
     loadCarousel(imageData);
     currentStatusClass = statusClass;
@@ -7940,6 +7942,7 @@ audioButton.forEach(button => {
 }
 
 function updateAqitoArabic() {
+
     document.querySelectorAll('.good-label-graph, .moderate-label-graph, .sensitive-label-graph, .unhealthy-label-graph, .veryunhealthy-label-graph, .hazardous-label-graph')
         .forEach(item => {
             switch (item.textContent.trim()) {
